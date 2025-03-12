d6f2f21e29c31e3e8f9ec534e8d01a5606a27a00
prakashrajan <prakashrajan2908@gmail.com>
2025-03-12 10:07:42 +0530

    new switch case coded from scratch

--- 7. SwitchCase-task/Switch.js
+++ 7. SwitchCase-task/Switch.js
@@ -1,18 +1,71 @@
 let input = require("prompt-sync")();
 
-let age = Number(input("Enter your age: "));
+let totalBill = 0;
+let orderNumber;
 
-let result;
+do {
+  console.log("~~~~~~~Welcome to CD Restaurant~~~~~~~");
+  const menu = {
+    1: "Biriyani - 150",
+    2: "Parotta - 100",
+    3: "Pizza - 200",
+    4: "SouthIndian - 120",
+    5: "Chinese - 100",
+    6: "Exit",
+  };
+  for (let [itemNo, item] of Object.entries(menu)) {
+    console.log(`${itemNo}: ${item}`);
+  }
 
-switch (true) {
-  case (age < 18):
-    result = "you're not eligible for voting";
-    break;
-  case (age >= 18):
-    result = "you're eligible for voting";
-    break
-  default:
-    result = "Enter correct age";
-}
+  let orderNumber = input("Enter your order number: ");
 
-console.log(result)
+  switch (orderNumber) {
+    case "1":
+      totalBill += 150;
+      console.log(
+        `\n\nYour order ${
+          menu[orderNumber].split(" ")[0]
+        } is ready and your bill is $${totalBill}\nThank you....Is there anything you want\n`
+      );
+      break;
+    case "2":
+      totalBill += 100;
+      console.log(
+        `\n\nYour order ${
+          menu[orderNumber].split(" ")[0]
+        } is ready and your bill is $${totalBill}\nThank you....Is there anything you want\n`
+      );
+      break;
+    case "3":
+      totalBill += 200;
+      console.log(
+        `\n\nYour order ${
+          menu[orderNumber].split(" ")[0]
+        } is ready and your bill is $${totalBill}\nThank you....Is there anything you want\n`
+      );
+      break;
+    case "4":
+      totalBill += 120;
+      console.log(
+        `\n\nYour order ${
+          menu[orderNumber].split(" ")[0]
+        } is ready and your bill is $${totalBill}\nThank you....Is there anything you want\n`
+      );
+      break;
+    case "5":
+      totalBill += 100;
+      console.log(
+        `\n\nYour order ${
+          menu[orderNumber].split(" ")[0]
+        } is ready and your bill is $${totalBill}\nThank you....Is there anything you want\n`
+      );
+      break;
+    case "6":
+      console.log(
+        `\n\nTotal bill: ${totalBill}\nExisting from menu Thank you!\n\n`
+      );
+      return;
+    default:
+      console.log("\n\nEnter correct menu number..");
+  }
+} while (orderNumber !== "6");
